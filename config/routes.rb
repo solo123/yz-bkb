ffc5bbd2913737eacb9b1728b25579f102795f86
Jimmy Liang <solo123@21cn.com>
2011-04-13 15:05:11 +0800

    dd

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Bkb::Application.routes.draw do
   root :to => 'home#index'
   match '/visit' => 'home#list'
-  match '/demo' => 'demo#detail'
+  match '/demo' => 'demo#details'
   match '/:all' => 'home#index', :constraints => { :all => /.+/ }
 end
