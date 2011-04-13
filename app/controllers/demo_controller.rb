c9b8d9fcbf461840292de766eb893e3d55ebec51
Jimmy Liang <solo123@21cn.com>
2011-04-13 17:10:47 +0800

    xml

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,7 +1,15 @@
 class DemoController < ApplicationController
 	respond_to :html, :xml 
 	def details
-		render 'details.xml.erb'
+		if params[:p]
+			if params[:p] == 'simplebooktype'
+				render 'book_list_brief.xml.erb'
+			else
+				render 'book_list.xml.erb'
+			end
+		elsif params[:bookid]
+			render 'book_detail.xml.erb'
+		end
 	end
 	def sync
 		render 'sync.xml.erb'
