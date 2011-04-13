3b8d05eaf08f89eb45a0a1f4b14e6bcb67e2f9db
Jimmy Liang <solo123@21cn.com>
2011-04-13 15:26:57 +0800

    xml

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Bkb::Application.routes.draw do
   root :to => 'home#index'
   match '/visit' => 'home#list'
-  match '/demo' => 'demo#details'
+  match '/vamp/mtkbook/details.aspx' => 'demo#details'
   match '/:all' => 'home#index', :constraints => { :all => /.+/ }
 end
