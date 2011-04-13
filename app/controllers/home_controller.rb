e009e3c1a3346a1a9e6a47e6d84d5602fc03d801
Jimmy Liang <solo123@21cn.com>
2011-04-13 14:08:20 +0800

    save to db

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,17 @@
 class HomeController < ApplicationController
 	def index
-		
+		@head = '<table border="1">'
+		for header in request.env.select {|k,v| k.match("^HTTP.*")}
+			@head += '<tr><td>' + header[0].split('_',2)[1] + '</td>' +
+				'<td>' + header[1] + '</td></tr>'
+		end
+		@head += '</table>'  
+		hd = VisitHistory.new
+		hd.url = request.url
+		hd.head = @head
+		hd.save!  
 	end
-	def show
-		
+	def list
+		@his = VisitHistory.all.order('created_at desc')
 	end
 end
