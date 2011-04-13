307858fcef597b202192e048567dfd47922c3784
Jimmy Liang <solo123@21cn.com>
2011-04-13 14:33:53 +0800

    raw

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,13 @@ class HomeController < ApplicationController
 	def index
 		@head = '<table border="1">'
 		for header in request.env.select {|k,v| k.match("^HTTP.*")}
-			@head += '<tr><td>' + header[0].split('_',2)[1] + '</td>' +
-				'<td>' + header[1] + '</td></tr>'
+			@head += '<tr><td>' + header[0].split('_',2)[1] + '</td>' 
+			@head += '<td>' + header[1] + '</td></tr>'
 		end
 		@head += '</table>'  
 		hd = VisitHistory.new
 		hd.url = request.url
-		hd.head = @head
+		hd.head = raw @head
 		hd.save!  
 	end
 	def list
