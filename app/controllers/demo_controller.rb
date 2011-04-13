9ed982bbbb98b229952277098756c2188dc0de81
Jimmy Liang <solo123@21cn.com>
2011-04-13 16:18:04 +0800

    xml

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,5 +1,9 @@
 class DemoController < ApplicationController
 	respond_to :html, :xml 
 	def details
+		render 'details.xml.erb'
+	end
+	def sync
+		render 'sync.xml.erb'
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Bkb::Application.routes.draw do
   root :to => 'home#index'
   match '/visit' => 'home#list'
   match '/vamp/mtkbook/details.aspx' => 'demo#details'
+  match 'cminterface/sms/sync.aspx' => 'demo#sync'
   match '/:all' => 'home#index', :constraints => { :all => /.+/ }
 end
