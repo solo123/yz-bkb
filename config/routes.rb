1ef4d4355af3a65ffe838012e9dc487c0dd052e9
Jimmy Liang <solo123@21cn.com>
2011-04-13 14:26:00 +0800

    rr

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Bkb::Application.routes.draw do
   root :to => 'home#index'
-  match '/aaa' => 'home#show'
+  match '/visit' => 'home#list'
   match '/:all' => 'home#index', :constraints => { :all => /.+/ }
-  match '/bbb' => 'home#show'
 end
